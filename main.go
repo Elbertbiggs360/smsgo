5e54b0ad8bdf4f7d56f276b1373e0fbd412ecfea
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2019-04-11 11:53:57 +0300

    arrays

--- main.go
+++ main.go
@@ -7,4 +7,8 @@ func main() {
 	var lastName string = "Bigirwenkya"
 	const gender string = "male"
 	fmt.Println("Hello, my name is", name, lastName)
+	// arrays
+	var arr [5]int // are a fixed size
+	arr[4] = 500   // their values must match the specified value data type
+	fmt.Println(arr)
 }
